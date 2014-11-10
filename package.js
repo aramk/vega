614810194e6af0c2a0b58b4ae02e1f7fbc7c732a
Aram Kocharyan <akarmenia@gmail.com>
2014-11-10 14:41:00 +1100

    Added Meteor dependency files.

---
+++ package.js
@@ -0,0 +1,14 @@
+// Meteor package definition.
+Package.describe({
+  name: 'aramk:vega',
+  version: '1.4.2',
+  summary: 'A grammar for charts with visualization using D3.',
+  git: 'https://github.com/aramk/vega.git#meteor'
+});
+
+Package.onUse(function (api) {
+  api.versionsFrom('METEOR@0.9.0');
+  api.use('aramk:d3', 'client');
+  api.export(['vg'], 'client');
+  api.addFiles(['vega.js'], 'client');
+});
