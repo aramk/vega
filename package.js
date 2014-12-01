fc8573ba32498b58edcd0ebc1f1b15075ad31eaa
Aram Kocharyan <akarmenia@gmail.com>
2014-12-01 23:51:01 +1100

    Migrated to d3js:d3.

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 // Meteor package definition.
 Package.describe({
   name: 'aramk:vega',
-  version: '1.4.2',
+  version: '1.4.2_1',
   summary: 'A grammar for charts with visualization using D3.',
   git: 'https://github.com/aramk/vega.git#meteor'
 });
 
 Package.onUse(function (api) {
   api.versionsFrom('METEOR@0.9.0');
-  api.use('aramk:d3@3.4.13', 'client');
+  api.use('d3js:d3@3.4.13', 'client');
   api.export(['vg'], 'client');
   api.addFiles(['vega.js'], 'client');
 });
